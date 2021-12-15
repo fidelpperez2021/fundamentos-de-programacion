10d002ab8a7d2bbaca7c472ae0cc6f47cd984e03
fidelpperez2021 <92316606+fidelpperez2021@users.noreply.github.com>
2021-12-15 11:49:30 -0500

    Create suma.cpp

---
+++ Mi actividad C-2/Pérez_Fidel/suma.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	// declaración de variables.
+	int NumeroA;
+	int NumeroB;
+	int Suma;
+	
+	//Solicitud de números enteros.
+	std::cout<<"Introduzca el primer entero:\n"; std::cin>>NumeroA;
+	std::cout<<"Introduzca el segundo entero:\n"; std::cin>>NumeroB;
+
+	//Suma de los dos dígitos
+	Suma=NumeroA+NumeroB;
+	std::cout<<"El resultado de la suma es:"<<Suma<<::std::endl;
+	
+	return 0;
+}
