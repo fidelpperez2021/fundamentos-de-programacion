e0476a28969e3c422eb24e1edfcbc6553f041476
fidelpperez2021 <92316606+fidelpperez2021@users.noreply.github.com>
2021-12-15 16:19:11 -0500

    Create suma.cpp

---
+++ Mi actividad C-2/Pérez_Fidel/estructura repetitiva/suma.cpp
@@ -0,0 +1,21 @@
+#include <iostream.h>
+#include <conio.h>
+main()
+{int a,b,c,d,e,f,suma;
+cout<<"\n ingrese el numero a:";
+cin>>a;
+cout<<"\n ingrese el numero b:";
+cin>>b;
+cout<<"\n ingrese el numero c:";
+cin>>c;
+cout<<"\n ingrese el numero d:";
+cin>>d;
+cout<<"\n ingrese el numero e:";
+cin>>e;
+cout<<"\n ingrese el numero f:";
+cin>>f;
+suma=a+b+c+d+e+f;
+cout<<"\n la suma de "<<a<<"+"<<b<<""<<c<<"+"<<d<<""<<e<<"+"<<f<<" es:"<<suma;
+getch();
+return 0;
+}
