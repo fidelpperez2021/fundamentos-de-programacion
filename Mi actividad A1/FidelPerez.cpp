032428638aafb90b71da276325d5e7bbf5275921
fidelpperez2021 <92316606+fidelpperez2021@users.noreply.github.com>
2021-12-17 19:00:46 -0500

    Delete FidelPerez.cpp

--- Mi actividad A1/FidelPerez.cpp
+++
@@ -1,8 +0,0 @@
-# incluye < iostream >                                         
-usando el  espacio de nombres  std ;                                       
-int  main ()                                                 
-{                                                                                                                             
-     
-       cout << " Hola mi nombre es Fidel Perez " ;                     
-       return  0 ;                                          
-}
