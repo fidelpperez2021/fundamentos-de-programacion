7d8b383195b5aa135c5ce85cd8c229b7f913bc50
Carlosbagui23 <carlos.bagui.ortiz@utelvt.edu.ec>
2021-12-19 00:06:33 -0500

    sumasimple

---
+++ Mi actividad C-2/carlos bagui/sumasimple.cpp
@@ -0,0 +1,16 @@
+//ejercicio de la suma de dos numeros//
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x,y,z;
+	cout<<"ingrese su primer numero: ";
+	cin>>x;
+	cout<<"ingrese su segundo numero: ";
+	cin>>y;
+	z=x+y;
+	cout<<"el resultado de la suma es: "<<z<<endl;
+	return 0;
+}
+
